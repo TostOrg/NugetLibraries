1f936b36718197eb4fea17b38e8ee195d53bcfdc
DESKTOP-5R07TP9\Dominik <dominik246@gmail.com>
2024-02-01 01:20:50 +0100

    directory.build.props fix

--- src/Tosts.CollectionsExtensions/CollectionExt.cs
+++ src/Tosts.CollectionsExtensions/CollectionExt.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
 
 namespace Tost.CollectionsExtensions;
 
-public static class Collection
+public static class CollectionExt
 {
     public static Collection<T> FindAll<T>(this Collection<T> collection, Predicate<T> predicate)
     {
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(predicate);
+
         var list = new List<T>(collection.Count);
 
         var counter = 0;
@@ -26,20 +30,26 @@ public static class Collection
         return new Collection<T>(list);
     }
 
-    public static Collection<U> ConvertAll<T, U>(this Collection<T> collection, Converter<T, U> converter)
+    public static Collection<TOutput> ConvertAll<TInput, TOutput>(this Collection<TInput> collection, Converter<TInput, TOutput> converter)
     {
-        var list = new List<U>(collection.Count);
-        
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(converter);
+
+        var list = new List<TOutput>(collection.Count);
+
         for (int i = 0; i < collection.Count; i++)
         {
             list[i] = converter(collection[i]);
         }
 
-        return new Collection<U>(list);
+        return new Collection<TOutput>(list);
     }
 
     public static T? Find<T>(this Collection<T> collection, Predicate<T> predicate)
     {
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(predicate);
+
         for (int i = 0; i < collection.Count; i++)
         {
             if (predicate(collection[i]) is false)
