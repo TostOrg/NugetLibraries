feda788a60e9f356a3bdb35ebd8f94ead1d97e79
DESKTOP-5R07TP9\Dominik <dominik246@gmail.com>
2024-02-13 23:54:57 +0100

    Added test case for Result WithValue

--- tests/Tost.ObjectResult.Tests/SuccessResultTests.cs
+++ tests/Tost.ObjectResult.Tests/SuccessResultTests.cs
@@ -13,4 +13,15 @@ public class SuccessResultTests
 
         okResult.Should().BeOfType<SuccessResult<int>>().Which.Value.Should().Be(value);
     }
+
+    [Fact]
+    public void ResultOkWithValueShouldReturnTypedSuccessResult()
+    {
+        const int value = 123;
+        var okResult = Result.Ok();
+        var typedOkResult = okResult.WithValue(value);
+
+        okResult.Should().BeOfType<SuccessResult>();
+        typedOkResult.Should().BeOfType<SuccessResult<int>>().Which.Value.Should().Be(value);
+    }
 }
